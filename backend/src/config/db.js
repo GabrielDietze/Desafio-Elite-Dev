a5e00a5caa2ebd6c2d58d955d3727d488e3eb407
GabrielDietze <gabrieldietzenovy@gmail.com>
2024-09-05 20:06:26 -0300

    chore(routes): remover códigos desnecessários

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,10 +6,7 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
     const uri = process.env.MONGODB_URI;
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(uri);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);

bbda2766abaf281d568bb866f88e70be327dec17
GabrielDietze <gabrieldietzenovy@gmail.com>
2024-09-06 11:14:12 -0300

    Update(movies): Adiciona a rota de buscar filme por categoria e buscar por genero, foi adicionado tambem o parametro with_networks para pegar filmes apenas da netflix

--- backend/src/routes/movies.js
+++ backend/src/routes/movies.js
@@ -8,6 +8,7 @@ const TMDB_API_KEY = process.env.TMDB_API_KEY;
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 const LANGUAGE_PARAM = 'pt-BR';
 
+//buscar filmes em cartaz
 router.get('/search/:query', async (req, res) => {
   const query = req.params.query;
   try {
@@ -15,7 +16,8 @@ router.get('/search/:query', async (req, res) => {
       params: {
         api_key: TMDB_API_KEY,
         query: query,
-        language: LANGUAGE_PARAM
+        language: LANGUAGE_PARAM,
+        with_networks: 213 // Netflix
       }
     });
     res.json(response.data);
@@ -24,13 +26,15 @@ router.get('/search/:query', async (req, res) => {
   }
 });
 
+//buscar detalhes do filme
 router.get('/movie/:id', async (req, res) => {
     const movieId = req.params.id;
     try {
       const response = await axios.get(`${TMDB_BASE_URL}/movie/${movieId}`, {
         params: {
           api_key: TMDB_API_KEY,
-          language: LANGUAGE_PARAM
+          language: LANGUAGE_PARAM,
+          with_networks: 213 // Netflix
         }
       });
       res.json(response.data);
@@ -38,5 +42,43 @@ router.get('/movie/:id', async (req, res) => {
       res.status(500).json({ error: 'Erro ao buscar detalhes do filme' });
     }
   });  
+
+
+  //buscar filmes por categoria
+router.get('/movie/:category', async (req, res) => {
+  const category = req.params.category; // Ex: 'top_rated', 'action', etc.
+  try {
+      const response = await axios.get(`${TMDB_BASE_URL}/movie/${category}`, {
+          params: {
+              api_key: TMDB_API_KEY,
+              language: LANGUAGE_PARAM,
+              with_networks: 213 // Netflix
+          }
+      });
+      res.json(response.data);
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao buscar filme por categoria');
+  }
+});
+
+// Rota para buscar filmes por gênero
+router.get('/movie/genre/:genreId', async (req, res) => {
+  const genreId = req.params.genreId; // ID do gênero
+  try {
+      const response = await axios.get(`${TMDB_BASE_URL}/discover/movie`, {
+          params: {
+              api_key: TMDB_API_KEY,
+              language: LANGUAGE_PARAM,
+              with_genres: genreId, // Filtra filmes por gênero
+              with_networks: 213 // Netflix
+          }
+      });
+      res.json(response.data);
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao buscar filmes por gênero');
+  }
+});
 
 module.exports = router;

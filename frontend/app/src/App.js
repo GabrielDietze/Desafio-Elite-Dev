9bc04e17981e07e0c363e0073ab34557f6cf0058
GabrielDietze <gabrieldietzenovy@gmail.com>
2024-09-09 09:23:34 -0300

    mudança nas rotas do app

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Auth/Login';
-import Home from './components/Home/Home';
-import MovieDetail from './components/MovieDetail/MovieDetail';
-import Favorites from './components/Favorites/Favorites';
-import Profile from './components/Profile/Profile';
+import './App.css';
+import Login from './components/Auth';
+import Home from './components/Home';
+import MovieDetail from './components/Movie/MovieDetail';
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
 
 function App() {
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/:type/:id" element={<MovieDetail />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>

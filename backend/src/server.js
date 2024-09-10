0eff3cfdd335bf582a312a3dce08d9c67f98b84c
meu pessoal <gabrieldietzenovy@gmail.com>
2024-09-10 17:41:05 -0300

    adicionado cors e porta do backend 5000

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const usersRouter = require('./routes/users');
 const connectDB = require('./config/db');
 const movieRouter = require('./routes/movies');
 const favoriteRouter = require('./routes/favorites');
 const sharedFavoritesRouter = require('./routes/SharedFavorites');
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
 
 connectDB();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(favoriteRouter);
 app.use(usersRouter);
 app.use(movieRouter);
 app.use(sharedFavoritesRouter);
 
-app.listen(3001, () => {
-  console.log('Server running on port 3001');
+app.listen(port, () => {
+  console.log('Server running on port ' + port);
 });

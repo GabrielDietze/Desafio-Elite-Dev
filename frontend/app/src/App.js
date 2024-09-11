01a255341922ace7bd7f79a8679225505e11beb7
GabrielDietze <gabrieldietzenovy@gmail.com>
2024-09-11 00:14:49 -0300

    modificado: Rotas do server.js agora tem o parametro protected routes

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Auth';
@@ -5,17 +6,18 @@ import Home from './components/Home';
 import MovieDetail from './components/Movie/MovieDetail';
 import Favorites from './components/Favorites';
 import Profile from './components/Profile';
+import ProtectedRoute from './components/Utils/ProtectedRoute';
 
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/:type/:id" element={<MovieDetail />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
+        <Route path="/:type/:id" element={<ProtectedRoute element={<MovieDetail />} />} />
+        <Route path="/favorites" element={<ProtectedRoute element={<Favorites />} />} />
+        <Route path="/profile" element={<ProtectedRoute element={<Profile />} />} />
       </Routes>
     </Router>
   );

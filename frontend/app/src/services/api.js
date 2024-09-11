2b354f7311a723a71514a0dddd6ac8cbd6d8c6cc
GabrielDietze <gabrieldietzenovy@gmail.com>
2024-09-11 00:13:30 -0300

    Adicionado chamada da api do back no frontend

--- frontend/app/src/services/api.js
+++ frontend/app/src/services/api.js
@@ -1,7 +1,26 @@
 import axios from 'axios';
 
+// Crie uma instância do Axios com a URL base
 const api = axios.create({
   baseURL: 'http://localhost:5000',
 });
+
+// Adicione um interceptor para todas as requisições
+api.interceptors.request.use(
+  config => {
+    // Tente obter o token do localStorage
+    const token = localStorage.getItem('authToken');
+    
+    if (token) {
+      // Se o token estiver disponível, adicione-o ao cabeçalho Authorization
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    // Se houver um erro na solicitação, rejeite a Promise
+    return Promise.reject(error);
+  }
+);
 
 export default api;

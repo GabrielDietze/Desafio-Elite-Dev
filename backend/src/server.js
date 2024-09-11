00b2e9fc119d2713aa2fc0f10a381b96e6b9b1cb
GabrielDietze <gabrieldietzenovy@gmail.com>
2024-09-11 00:12:23 -0300

    modificado(server.js): rotas alteradas

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,9 @@ const usersRouter = require('./routes/users');
 const connectDB = require('./config/db');
 const movieRouter = require('./routes/movies');
 const favoriteRouter = require('./routes/favorites');
-const sharedFavoritesRouter = require('./routes/SharedFavorites');
+const authMiddleware = require('./middleware/authMiddleware');
 require('dotenv').config()
+
 
 const port = process.env.PORT || 5000;
 
@@ -15,10 +16,12 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 
-app.use(favoriteRouter);
 app.use(usersRouter);
-app.use(movieRouter);
-app.use(sharedFavoritesRouter);
+app.use(favoriteRouter, authMiddleware);
+app.use(movieRouter, authMiddleware);
+
+
+
 
 app.listen(port, () => {
   console.log('Server running on port ' + port);

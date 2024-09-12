70e004d960feae030f4e9fa2140b6232df4e55e0
GabrielDietze <gabrieldietzenovy@gmail.com>
2024-09-12 01:06:07 -0300

    fix: corrige as rotas de favoritos, ajustando endpoints e parâmetros para melhorar a funcionalidade e a segurança

--- backend/src/routes/favorites.js
+++ backend/src/routes/favorites.js
@@ -1,21 +1,21 @@
-    const express = require('express');
-    const router = express.Router();
-    const favoritesController = require('../controllers/FavoritesController');
-    const SharedFavoritesController = require('../controllers/SharedFavoritesController');
+const express = require('express');
+const router = express.Router();
+const favoritesController = require('../controllers/FavoritesController');
+const SharedFavoritesController = require('../controllers/SharedFavoritesController');
 
-    // Buscar a lista de favoritos de um usuário
-    router.get('/favorites/:userId', favoritesController.index);
+// Buscar a lista de favoritos de um usuário
+router.get('/favorites/:userId', favoritesController.index);
 
-    // Adicionar um filme à lista de favoritos
-    router.post('/favorites', favoritesController.store);
+// Adicionar uma mídia (filme ou série) à lista de favoritos
+router.post('/favorites', favoritesController.store);
 
-    // Remover um filme da lista de favoritos
-    router.delete('/favorites', favoritesController.delete);
+// Remover uma mídia da lista de favoritos
+router.delete('/favorites', favoritesController.delete);
 
-    // Gerar um link compartilhável para a lista de favoritos
-    router.post('/favorites/shared/:userId', SharedFavoritesController.createShareLink);
+// Gerar um link compartilhável para a lista de favoritos
+router.post('/favorites/shared/:userId', SharedFavoritesController.createShareLink);
 
-    // Acessar a lista de favoritos via link compartilhado
-    router.get('/favorites/shared/:token', SharedFavoritesController.getFavoritesByShareLink);
+// Acessar a lista de favoritos via link compartilhado
+router.get('/favorites/shared/:token', SharedFavoritesController.getFavoritesByShareLink);
 
-    module.exports = router;
+module.exports = router;

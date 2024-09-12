f09a49e95045f231a19968996ce093737f6d6031
GabrielDietze <gabrieldietzenovy@gmail.com>
2024-09-12 15:35:25 -0300

    fix: corrigir problemas no componente principal da aplicação e melhorar a integração com outros componentes

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -4,16 +4,18 @@ import './App.css';
 import Login from './components/Auth';
 import Home from './components/Home';
 import MovieDetail from './components/Movie/MovieDetail';
-import Favorites from './components/Favorites';
+import Favorites from './components/Favorites/FavoritesPage/';
 import Profile from './components/Profile';
 import ProtectedRoute from './components/Utils/ProtectedRoute';
+import SharedFavorites from './components/Favorites/SharedFavorites/SharedPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<ProtectedRoute element={<Home />} />} />
+        <Route path="/favorites/shared/:token" element={<ProtectedRoute element={<SharedFavorites />} />} />
         <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
         <Route path="/:type/:id" element={<ProtectedRoute element={<MovieDetail />} />} />
         <Route path="/favorites" element={<ProtectedRoute element={<Favorites />} />} />

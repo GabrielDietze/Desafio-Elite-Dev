851dc152e26ece05b1ab5c50f9b859331d95ace6
GabrielDietze <gabrieldietzenovy@gmail.com>
2024-09-12 15:35:26 -0300

    fix: refatorar componente de detalhes do filme para melhorar a estrutura e a lógica

--- frontend/app/src/components/Movie/MovieDetail/index.js
+++ frontend/app/src/components/Movie/MovieDetail/index.js
@@ -7,11 +7,15 @@ import api from '../../../services/api';
 import FavoriteButton from '../../Utils/FavoriteButton/FavoriteButton';
 
 const MovieDetail = () => {
-    const { type, id } = useParams(); // Pega o parâmetro type e id da URL
+    // Obtém os parâmetros da URL (tipo e id)
+    const { type, id } = useParams();
+    // Estado para armazenar as informações do item
     const [item, setItem] = useState(null);
+    // Estado para gerenciar o cabeçalho preto
     const [blackHeader, setBlackHeader] = useState(false);
 
     useEffect(() => {
+        // Função para verificar a rolagem da página e definir o cabeçalho preto
         const scrollListener = () => {
             if (window.scrollY > 10) {
                 setBlackHeader(true);
@@ -20,77 +24,92 @@ const MovieDetail = () => {
             }
         }
 
+        // Adiciona o ouvinte de rolagem
         window.addEventListener('scroll', scrollListener);
 
+        // Remove o ouvinte de rolagem ao desmontar o componente
         return () => {
             window.removeEventListener('scroll', scrollListener);
         }
     }, []);
 
     useEffect(() => {
+        // Função assíncrona para buscar os detalhes do item
         const fetchItem = async () => {
-            try {
                 const response = await api.get(`/${type}/${id}`);
                 setItem(response.data);
-            } catch (error) {
-                console.error('Erro ao buscar detalhes do filme:', error);
-            }
         };
         fetchItem();
     }, [type, id]);
 
+    // Exibe um carregando enquanto os dados não são obtidos
     if (!item) {
-        return   <div className='loading'>
+        return <div className='loading'>
             <img src="https://media1.tenor.com/m/UnFx-k_lSckAAAAC/amalie-steiness.gif" alt="Carregando" />
             </div>
-        
     }
 
+    // Gera uma lista de gêneros
     const genres = item.genres ? item.genres.map((genre) => genre.name).join(', ') : '';
 
-       
     return (
         <>
             <div className='container-movie-detail'>
-            <div className='header-movie-detail'>
-            <Header black={blackHeader}/> 
-            </div>
-            
-            <section className="movie" style={{
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundImage: `linear-gradient(to right, #111 30%, transparent 70%), linear-gradient(to top, rgba(0, 0, 0, 0.7) 10%, transparent 90%), url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
-            }}>
-                <div className='back-button'>
-                <BackButton />
+                <div className='header-movie-detail'>
+                    {/* Renderiza o cabeçalho com base no estado blackHeader */}
+                    <Header black={blackHeader} /> 
                 </div>
-                <div className="featured-name"> {item.title || item.original_name} </div>
-                <div className="featured-info">
-                <div className="featured-points">
-                     Nota: {item.vote_average ? item.vote_average.toFixed(1) : 'N/A'}
+                
+                <section className="movie" style={{
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundImage: `linear-gradient(to right, #111 30%, transparent 70%), linear-gradient(to top, rgba(0, 0, 0, 0.7) 10%, transparent 90%), url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+                }}>
+                    <div className='back-button'>
+                        {/* Botão para voltar */}
+                        <BackButton />
                     </div>
-                    <div className="featured-year">
-                        {new Date(item.release_date || item.first_air_date).getFullYear()}
+                    <div className="featured-name">
+                        {/* Nome do filme ou série */}
+                        {item.title || item.original_name}
                     </div>
-                    {type === 'tv' && item.number_of_seasons && (
-                        <div className="featured-seasons">
-                            {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}
+                    <div className="featured-info">
+                        <div className="featured-points">
+                             {/* Nota do filme ou série */}
+                             Nota: {item.vote_average ? item.vote_average.toFixed(1) : 'N/A'}
+                        </div>
+                        <div className="featured-year">
+                            {/* Ano de lançamento */}
+                            {new Date(item.release_date || item.first_air_date).getFullYear()}
+                        </div>
+                        {type === 'tv' && item.number_of_seasons && (
+                            <div className="featured-seasons">
+                                {/* Número de temporadas (para séries) */}
+                                {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}
+                            </div>
+                        )}
+                        {type === 'movie' && item.runtime && (
+                            <div className="featured-runtime"> 
+                                {/* Duração do filme (para filmes) */}
+                                {item.runtime} minutos 
+                            </div>
+                        )}
+                    </div>
+                    <div className="movie-description">
+                        {/* Descrição do filme ou série */}
+                        {item.overview}
+                    </div>
+                    <div className="featured-buttons">
+                        {/* Botão para adicionar aos favoritos */}
+                        <FavoriteButton item={item} />
+                    </div>
+                    {genres && (
+                        <div className="featured-genres">
+                            {/* Gêneros do filme ou série */}
+                            <strong>{item.genres.length > 1 ? 'Gêneros' : 'Gênero'}:</strong> {genres}
                         </div>
                     )}
-                    {type === 'movie' && item.runtime && (
-                        <div className="featured-runtime"> {item.runtime} minutos </div>
-                    )}
-                </div>
-                <div className="movie-description"> {item.overview} </div>
-                <div className="featured-buttons">
-                  <FavoriteButton item={item} />
-                </div>
-                {genres && (
-                    <div className="featured-genres">
-                        <strong>{item.genres.length > 1 ? 'Gêneros' : 'Gênero'}:</strong> {genres}
-                    </div>
-                )}
-            </section>
+                </section>
             </div>
         </>
     );

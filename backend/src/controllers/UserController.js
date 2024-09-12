5eebe3f747dce09b558f2d9533ae91950bcfb40e
GabrielDietze <gabrieldietzenovy@gmail.com>
2024-09-12 15:38:20 -0300

    fix: refatorar controlador de usuários para corrigir problemas e melhorar a manipulação de dados dos usuários

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,33 +1,44 @@
+// Importa o módulo bcryptjs para criptografia de senhas
 const bcrypt = require('bcryptjs');
+// Importa o modelo User para manipulação de dados de usuários
 const User = require('../models/user');
+// Importa o módulo jsonwebtoken para criação e verificação de tokens JWT
 const jwt = require('jsonwebtoken');
+// Carrega variáveis de ambiente do arquivo .env
 require('dotenv').config();
 
+// Obtém o segredo para criar tokens JWT a partir das variáveis de ambiente
 const JWT_SECRET = process.env.JWT_SECRET;
 
-// Login do usuário
+// Função para fazer login do usuário
 exports.login = async (req, res) => {
+  // Desestrutura as propriedades username e password do corpo da requisição
   const { username, password } = req.body;
 
   try {
+    // Encontra um usuário pelo nome de usuário fornecido
     const user = await User.findOne({ username });
     if (!user) {
       return res.status(404).json({ msg: 'Usuário não encontrado' });
     }
 
+    // Compara a senha fornecida com a senha armazenada no banco de dados
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ msg: 'Senha inválida' });
     }
 
+    // Cria o payload para o token JWT com as informações do usuário
     const payload = {
       user: {
         id: user.id,
         username: user.username,
       }
     };
+    // Gera um token JWT com o payload e o segredo
     const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
 
+    // Retorna o token e o ID do usuário na resposta
     res.json({ token, userId: user.id });
   } catch (err) {
     console.error(err.message);
@@ -35,31 +46,32 @@ exports.login = async (req, res) => {
   }
 };
 
-// Registrar um novo usuário
+// Função para registrar um novo usuário
 exports.register = async (req, res) => {
+  // Desestrutura as propriedades username, email e password do corpo da requisição
   const { username, email, password } = req.body;
 
   try {
-    // Verificar se o e-mail já está em uso
+    // Verifica se o e-mail já está em uso
     let user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ msg: 'E-mail já está em uso' });
     }
 
-    // Verificar se o nome de usuário já está em uso
+    // Verifica se o nome de usuário já está em uso
     user = await User.findOne({ username });
     if (user) {
       return res.status(400).json({ msg: 'Nome de usuário já está em uso' });
     }
 
-    // Criar uma nova instância de usuário
+    // Cria uma nova instância de usuário
     user = new User({ username, email, password });
 
-    // Hashear a senha antes de salvar
+    // Gera um salt e hasheia a senha antes de salvar
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(password, salt);
 
-    // Salvar o usuário no banco de dados
+    // Salva o usuário no banco de dados
     await user.save();
 
     res.status(201).json({ msg: 'Usuário registrado com sucesso' });
@@ -69,13 +81,13 @@ exports.register = async (req, res) => {
   }
 };
 
-
-// Buscar um usuário pelo nome de usuário
+// Função para buscar um usuário pelo nome de usuário
 exports.getUserByUsername = async (req, res) => {
+  // Obtém o nome de usuário dos parâmetros da requisição
   const { username } = req.params;
 
   try {
-    // Encontrar o usuário pelo nome de usuário
+    // Encontra o usuário pelo nome de usuário fornecido
     const user = await User.findOne({ username });
 
     if (!user) {
@@ -89,20 +101,21 @@ exports.getUserByUsername = async (req, res) => {
   }
 };
 
-// Atualizar um usuário pelo nome de usuário
+// Função para atualizar um usuário pelo nome de usuário
 exports.updateUserByUsername = async (req, res) => {
+  // Obtém o nome de usuário dos parâmetros e as novas informações do corpo da requisição
   const { username } = req.params;
   const { newUsername, email, password } = req.body;
 
   try {
-      // Encontrar o usuário pelo nome de usuário atual
+      // Encontra o usuário pelo nome de usuário atual
       const user = await User.findOne({ username });
 
       if (!user) {
           return res.status(404).json({ message: 'Usuário não encontrado' });
       }
 
-      // Verificar se o novo nome de usuário já está em uso
+      // Verifica se o novo nome de usuário já está em uso e atualiza se necessário
       if (newUsername && newUsername !== username) {
           const existingUser = await User.findOne({ username: newUsername });
           if (existingUser) {
@@ -111,7 +124,7 @@ exports.updateUserByUsername = async (req, res) => {
           user.username = newUsername;
       }
 
-      // Verificar se o e-mail já está em uso
+      // Verifica se o e-mail já está em uso e atualiza se necessário
       if (email && email !== user.email) {
           const existingEmailUser = await User.findOne({ email });
           if (existingEmailUser) {
@@ -120,12 +133,13 @@ exports.updateUserByUsername = async (req, res) => {
           user.email = email;
       }
 
-      // Atualizar a senha se fornecida
+      // Atualiza a senha se fornecida
       if (password) {
           const hashedPassword = await bcrypt.hash(password, 10);
           user.password = hashedPassword;
       }
 
+      // Salva as alterações no banco de dados
       await user.save();
 
       res.status(200).json({ message: 'Usuário atualizado com sucesso' });
@@ -135,13 +149,13 @@ exports.updateUserByUsername = async (req, res) => {
   }
 };
 
-
-// Apagar um usuário pelo e-mail
+// Função para apagar um usuário pelo e-mail
 exports.deleteUserByEmail = async (req, res) => {
+  // Obtém o e-mail dos parâmetros da requisição
   const { email } = req.params;
 
   try {
-    // Encontrar e remover o usuário pelo e-mail
+    // Encontra e remove o usuário pelo e-mail
     const user = await User.findOneAndDelete({ email });
 
     if (!user) {

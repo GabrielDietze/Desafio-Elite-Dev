cf6526ececfac86b1be5a19929e37a88ad6d1500
GabrielDietze <gabrieldietzenovy@gmail.com>
2024-09-13 19:21:06 -0300

    Modificado, alteração na ordem das rotas para organização apenas

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
 // src/App.js
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './components/Auth';
 import Home from './components/Home';
+import Login from './components/Auth';
+import Profile from './components/Profile';
 import MovieDetail from './components/Movie/MovieDetail';
 import Favorites from './components/Favorites/FavoritesPage/';
-import Profile from './components/Profile';
 import ProtectedRoute from './components/Utils/ProtectedRoute';
 import SharedFavorites from './components/Favorites/SharedFavorites/SharedPage';
 
@@ -15,11 +15,11 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<ProtectedRoute element={<Home />} />} />
-        <Route path="/favorites/shared/:token" element={<ProtectedRoute element={<SharedFavorites />} />} />
         <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
-        <Route path="/:type/:id" element={<ProtectedRoute element={<MovieDetail />} />} />
-        <Route path="/favorites" element={<ProtectedRoute element={<Favorites />} />} />
         <Route path="/profile" element={<ProtectedRoute element={<Profile />} />} />
+        <Route path="/favorites" element={<ProtectedRoute element={<Favorites />} />} />
+        <Route path="/:type/:id" element={<ProtectedRoute element={<MovieDetail />} />} />
+        <Route path="/favorites/shared/:token" element={<ProtectedRoute element={<SharedFavorites />} />} />
       </Routes>
     </Router>
   );
